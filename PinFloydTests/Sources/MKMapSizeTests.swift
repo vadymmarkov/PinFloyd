01eacf04bd3db96196aae8d9d4744f8f1f51c828
Vadym Markov <markov.vadym@gmail.com>
2017-04-04 01:19:12 +0200

    Test map size

---
+++ PinFloydTests/Sources/MKMapSizeTests.swift
@@ -0,0 +1,20 @@
+import XCTest
+import MapKit
+@testable import PinFloyd
+
+class MKMapSizeTests: XCTestCase {
+
+  var mapSize: MKMapSize!
+
+  override func setUp() {
+    super.setUp()
+    mapSize = MKMapSize(width: 50, height: 100)
+  }
+
+  func testHalfSize() {
+    let halfSize = mapSize.halfSize
+
+    XCTAssertEqual(halfSize.width, 25)
+    XCTAssertEqual(halfSize.height, 50)
+  }
+}
